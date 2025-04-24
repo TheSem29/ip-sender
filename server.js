f25c5b87ad34b83033d1e618d16ffacf6878c3ea
TheSem29 <semsemsem1541@gmail.com>
2025-04-24 16:03:29 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+
+// Настройки почты (замени на свои, если вдруг у тебя есть почта)
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'aaaaaaaaaaaaaaaa1541@gmail.com', // Тут твой email (если он есть)
+        pass: '1801_angel' // Тут твой пароль (но ты его, конечно, забыл)
+    }
+});
+
+app.get('/', (req, res) => {
+    const ip = req.ip || req.connection.remoteAddress;
+    
+    const mailOptions = {
+        from: 'aaaaaaaaaaaaaaaa1541@gmail.com',
+        to: 'aaaaaaaaaaaaaaaa1541@gmail.com', // Куда слать (опять же, если ты не забыл)
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.send(`Error: ${error.message}`);
+        } else {
+            res.send('Thx for test');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
